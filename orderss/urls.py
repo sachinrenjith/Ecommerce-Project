60e855d019387fa99a9e4a49338c75a9ee06ceb8
Sachin Renjith <sachinranjith012gmail.com>
2023-07-24 21:46:53 +0530

    uncomment cleared

--- orderss/urls.py
+++ orderss/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 urlpatterns = [
     path('place_order/',views.place_order, name='place_order'),
-    path('payment/',views.payment,name='payment'),
     path('success/',views.success,name='success'),
-    # path('order_complete/',views.order_complete, name='order_complete'),
+
      
 
     
--- orderss/views.py
+++ orderss/views.py
@@ -11,65 +11,6 @@ from carts.models import Product
 from django.db import transaction
 
 # Create your views here.
-
-# @transaction.atomic
-def payment(request):
-    # Assuming you receive the payment details as POST data
-    razorpay_payment_id = request.GET.get("razorpay_payment_id")
-    amount_paid = request.GET.get("amount_paid")
-    razor_pay_status = request.GET.get("razor_pay_status")
-    order_number = request.GET.get("order_number")
-    print('llllllllllllllllllllllllllllllll')
-    print(razor_pay_status,amount_paid,razorpay_payment_id)
-     # Fetch the order based on the order_id and other relevant criteria
-    order = get_object_or_404(Order, user=request.user, is_ordered=False, order_number=order_number)
-    print(order)
-
-    # Storing Transaction Details In Payment Model
-    payment = Payment.objects.create(
-        user=request.user,
-        razorpay_payment_id=razorpay_payment_id,
-        amount_paid=amount_paid,
-        razorpay_status=razor_pay_status,
-    )
-    # payment.save()
-
-    # Update the order's payment and is_ordered status
-    order.payment = payment
-    order.is_ordered = True
-    order.save()
-    
-      # Move The Cart Items To Order Product Table
-    cart_items = CartItem.objects.filter(user=request.user)
-
-    for item in cart_items:
-        orderproduct = OrderProduct()
-        orderproduct.order_id = order.id
-        orderproduct.payment = payment
-        orderproduct.user_id = request.user.id
-        orderproduct.product_id = item.product_id
-        orderproduct.quantity = item.quantity
-        orderproduct.product_price = item.product.price
-        orderproduct.ordered = True
-        orderproduct.save()
-
-        # Reduce The Qantity Of The Sold Products
-        product = Product.objects.get(id=item.product_id)
-        product.stock -= item.quantity
-        product.save()
-
- 
-
-    # Clear Cart
-    CartItem.objects.filter(user=request.user).delete()
-
-    return render(request,'orders/payment.html')
-
-
-
-
-
-
 def place_order(request, total=0, quantity=0):
     current_user = request.user
      
@@ -143,67 +84,7 @@ def place_order(request, total=0, quantity=0):
         else:    
             return redirect('checkout')
 
-
-# def success(request):
-    
-#     # Get the payment details from the request
-#     razorpay_payment_id = request.GET.get("razorpay_payment_id")
-#     razorpay_order_id = request.GET.get("razorpay_order_id")
-#     razor_pay_status = request.GET.get("razor_pay_status")
-#     amount_paid = request.GET.get("amount_paid")
-#     order_number = request.GET.get("order_number")
-#     full_name = request.GET.get("full_name")
-#     full_address = request.GET.get("full_address")
-#     state = request.GET.get("state")
-#     city= request.GET.get("city")
-
-     
-#       # Storing Transaction Details In Payment Model
-#     payment = Payment.objects.create(
-#         user=request.user,
-#         razorpay_payment_id=razorpay_payment_id,
-#         amount_paid=amount_paid,
-#         razor_pay_status='Success',
-#         razorpay_order_id=order_number
-#     )
-
-#     order = get_object_or_404(Order, user=request.user, is_ordered=False, order_number=order_number)
-#     order.payment = payment
-#     order.is_ordered = True
-#     order.save()
-
-
-#      # Move The Cart Items To Order Product Table
-#     cart_items = CartItem.objects.filter(user=request.user)
-
-#     for item in cart_items:
-#         orderproduct = OrderProduct()
-#         orderproduct.order_id = order.id
-#         orderproduct.payment = payment
-#         orderproduct.user_id = request.user.id
-#         orderproduct.product_id = item.product_id
-#         orderproduct.quantity = item.quantity
-#         orderproduct.product_price = item.product.price
-#         orderproduct.ordered = True
-#         orderproduct.save()
-
-#         # Reduce The Qantity Of The Sold Products
-#         product = Product.objects.get(id=item.product_id)
-#         product.stock -= item.quantity
-#         product.save()
-   
-
-#     context = {
-#         "order_number": order_number,
-#         "amount_paid": amount_paid,
-#         "razorpay_payment_id": razorpay_payment_id,
-#         "full_name": full_name,
-#         "full_address":  full_address,
-#         "state": state,
-#         "city": city,
-        
-#     }
-#     return render(request, "orders/order_complete.html", context)
+ 
 
 def success(request):
       razorpay_payment_id = request.GET.get("razorpay_payment_id")
